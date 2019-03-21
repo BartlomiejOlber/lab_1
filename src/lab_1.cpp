ccd2f11eca52dc5393e125172d75a2b93260db42
BartlomiejOlber <48100983+BartlomiejOlber@users.noreply.github.com>
2019-03-21 11:10:12 +0100

    Update lab_1.cpp

--- src/lab_1.cpp
+++ src/lab_1.cpp
@@ -6,10 +6,6 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-#include "domain/model/smart_product_exception.hpp"
-#include "domain/model/stock.hpp"
-#include "infrastructure/stock_repository.hpp"
 #include "UI/main_options.hpp"
 
 
@@ -18,14 +14,7 @@ using namespace std;
 
 int main() {
 
-/*	domain::model::Stock stock;
-	infrastructure::StockRepository stock_repository( "products.txt" );
-	stock_repository.load( stock );
-	stock.print();
-*/
-
 	UI::MainOptions main_options;
-	//main_options.display();
 	main_options.loop_controler();
 
 	return 0;
